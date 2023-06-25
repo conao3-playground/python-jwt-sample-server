1d0958416ca65ed679cd36cb5d77d70fd281768b
Naoya Yamashita <conao3@gmail.com>
2023-06-25 10:59:55 +0900

    add simple server

--- src/jwt_sample_server/__main__.py
+++ src/jwt_sample_server/__main__.py
@@ -1,2 +1,13 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def root():
+    return {'message': 'Hello World'}
+
+
 def main():
-    print('hello')
+    uvicorn.run('jwt_sample_server.__main__:app', reload=True)
